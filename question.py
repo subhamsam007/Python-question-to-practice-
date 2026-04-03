6898f859f593bbb7414cf44d5de088e9db543f47
Your Name <mondalsubham801@gmail.com>
2026-04-03 17:38:21 +0530

    Q7. Write a program to calculate the factorial of a number using a loop.

--- question.py
+++ question.py
@@ -58,3 +58,12 @@ print(celsius_to_fahrengeit())
 
 
 #  Q7. Write a program to calculate the factorial of a number using a loop. 
+# def factorial(n):
+#     result = 1
+#     for i in range(1, n + 1):
+#         result *= i
+#     return result
+
+# Example usage:
+# num = int(input("Enter a number: "))
+# print("Factorial of", num, "is", factorial(num))
